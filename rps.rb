4ec87538e7689dcd24f4d9a651f3f7111c8f839b
Kellyx1636 <kellyx1636@hstat.org>
2017-10-24 12:24:44 +0000

    Starting off with a pseudocode.

---
+++ rps.rb
@@ -0,0 +1,22 @@
+# returns either "rock", "paper", or "scissors" using rand()
+def comp_choose_rps
+	rand_num = rand(3) 
+	
+end
+
+# returns either "rock", "paper", or "scissors" using gets.chomp
+def user_choose_rps
+	
+end
+
+# returns either "It was a tie!", "You win!", or "The computer wins!"
+def get_winner(comp,user)
+
+end
+
+# puts "The computer chose ____" followed by "You chose ____" followed by the result of #get_winner
+def play_game
+	
+end
+
+play_game
