9ea1f2d8a752b052de2671ac2548a3af79723a97
Kellyx1636 <kellyx1636@hstat.org>
2017-10-24 12:27:31 +0000

    Finished with get_winner method.

--- rps.rb
+++ rps.rb
@@ -11,7 +11,15 @@ end
 
 # returns either "It was a tie!", "You win!", or "The computer wins!"
 def get_winner(comp,user)
-
+   if (comp =="rock" && user == "scissors") || (comp =="paper" && user == "rock") || (comp =="scissors" && user == "rock")
+       return "The computer wins!"
+   elsif (comp =="rock" && user == "rock") || (comp =="scissors" && user == "scissors") || (comp =="paper" && user == "paper") 
+       return "It was a tie!"
+   elsif (comp =="rock" && user == " ") || (comp =="paper" && user == " ") || (comp =="scissors" && user == " ") 
+       return "What happen? Choose one already!"
+   else 
+       return "You win!"
+   end
 end
 
 # puts "The computer chose ____" followed by "You chose ____" followed by the result of #get_winner
