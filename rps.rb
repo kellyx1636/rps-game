950640d7d4ca49d29a7afc9c7f625d8b1fed58f4
Kellyx1636 <kellyx1636@hstat.org>
2017-10-24 17:22:02 +0000

    Tried switching the order of first two methods, failed.

--- rps.rb
+++ rps.rb
@@ -1,3 +1,9 @@
+# returns either "rock", "paper", or "scissors" using gets.chomp
+def user_choose_rps
+	puts "What would you like to put in? Rock? Paper? Scissors?" 
+	user = gets.chomp
+end
+
 # returns either "rock", "paper", or "scissors" using rand()
 def comp_choose_rps
 	rand_num = rand(3) 
@@ -10,25 +16,19 @@ def comp_choose_rps
 	end
 end
 
-# returns either "rock", "paper", or "scissors" using gets.chomp
-def user_choose_rps
-	puts "What would you like to put in? Rock? Paper? Scissors?" 
-	user = gets.chomp
-end
-
 # returns either "It was a tie!", "You win!", or "The computer wins!"
 def get_winner(comp,user)
    if (comp =="rock" && user == "scissors") || (comp =="paper" && user == "rock") || (comp =="scissors" && user == "rock")
        return "The computer wins!"
    elsif (comp =="rock" && user == "rock") || (comp =="scissors" && user == "scissors") || (comp =="paper" && user == "paper") 
        return "It was a tie!"
-#   elsif (comp =="rock" && user == " ") || (comp =="paper" && user == " ") || (comp =="scissors" && user == " ") 
-#       return "What happen? Choose one already!"
+   #  elsif (comp =="rock" && user == " ") || (comp =="paper" && user == " ") || (comp =="scissors" && user == " ") 
+   #      return "What happen? Choose one already!"
    # else #if the user does not give an answer 
    #    puts "Why didn't you enter your choice?"    
    else 
        return "You win!"
-   end
+   end 
 end
 
 # puts "The computer chose ____" followed by "You chose ____" followed by the result of #get_winner
@@ -38,4 +38,4 @@ def play_game
 	get_winner 
 end
 
-#play_game
+play_game
