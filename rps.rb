9b8bbdb0ec9e39e7c0b872f5f79bc81c309bb772
Kellyx1636 <kellyx1636@hstat.org>
2017-10-25 09:54:02 +0000

    Added a replay method.

--- rps.rb
+++ rps.rb
@@ -30,13 +30,29 @@ def get_winner(comp,user)
    end 
 end 
 
+# asking whether the user wants to play again 
+def replay 
+	puts "Would you like to play again? Please respond with yes or no." 
+	answer = gets.chomp 
+   	if answer == "yes" 
+		puts "Ready?...Go!"
+		play_game
+	elsif answer == "no"
+		puts "Alright then. Thank you for playing! OuO/"
+    else 
+    	puts "You didn't put neither 'yes' or 'no'. Let me ask you again."
+    	replay
+    end
+end
+
 # puts "The computer chose ____" followed by "You chose ____" followed by the result of #get_winner
 def play_game
 	#user_choose_rps 
 	comp_choice = comp_choose_rps #<-- no need to use this because it's used once 
 	user_choice = user_choose_rps  
 	puts "The computer chose #{comp_choice}. You chose #{user_choice}." 
-	puts get_winner(comp_choice, user_choice)
+	puts get_winner(comp_choice, user_choice) 
+	replay 
 end
 
 play_game
